082f9f4738b340c8ca36432fedb728620f38aa32
Olga Vesnina <venina29olga@gmail.com>
2024-02-16 00:35:47 +0300

    task1.1.3 done

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,7 +1,11 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -12,31 +16,67 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        try (Session session = Util.getSession().openSession()) {
+            session.beginTransaction();
+            session.createSQLQuery("CREATE TABLE IF NOT EXISTS ex1" +
+                    "(id INT NOT NULL AUTO_INCREMENT, " +
+                    "name VARCHAR(45) NOT NULL, " +
+                    "lastName VARCHAR(45) NOT NULL, " +
+                    "age INT(3) NOT NULL," +
+                    "PRIMARY KEY(id))").executeUpdate();
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try (Session session = Util.getSession().openSession()) {
+            session.beginTransaction();
+            session.createSQLQuery("DROP TABLE IF EXISTS ex1").executeUpdate();
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Session session = Util.getSession().openSession()) {
+            session.beginTransaction();
+            User user = new User(name, lastName, age);
+            session.save(user);
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try (Session session = Util.getSession().openSession()) {
+            session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.delete(user);
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> users;
+
+        try (Session session = Util.getSession().openSession()) {
+            session.beginTransaction();
+            users = session.createQuery("from User").getResultList();
+            session.getTransaction().commit();
+        }
+        return users;
     }
+
+    ;
 
     @Override
     public void cleanUsersTable() {
-
+        try (Session session = Util.getSession().openSession()) {
+            session.beginTransaction();
+            session.createQuery("delete User").executeUpdate();
+            session.getTransaction().commit();
+        }
     }
 }
